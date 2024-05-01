1a50f4779e8891c4eabf04db16d39cc0275b5c82
hduy0407 <t3ngdu01@students.oamk.fi>
2024-05-01 05:59:31 +0300

    update registration and logout

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const {query} = require('../helpers/db.js')
+const bcrypt = require('bcrypt')
 
 const userRouter = express.Router()
 
@@ -8,9 +9,20 @@ userRouter.post("/login", async(req, res)=> {
         const sql = "select * from account where email=$1"
         const result = await query(sql, [req.body.email])
         if (result.rowCount === 1) {
-            if (result.rows[0].password === req.body.password) {
-                res.status(200).json(result.rows[0])
-            }
+            bcrypt.compare(req.body.password, result.rows[0].password,(err,b_res) => {
+                if (!err) {
+                    if (b_res === true) {
+                        const user = result.rows[0]
+                        res.status(200).json({"id": user.id, "email": user.email})
+                    } else {
+                        res.statusMessage = 'Invalid login'
+                        res.status(401).json({error: 'Invalid login'})
+                    }
+                } else {
+                    res.statusMessage = err
+                    res.status(500).json({error: err})
+                }
+            })
         } else {
             res.statusMessage = 'Invalid login'
             res.status(401).json({error: 'Invalid login'})
@@ -21,15 +33,23 @@ userRouter.post("/login", async(req, res)=> {
     }
 })
 
-userRouter.post("/signup", async(req, res)=> {
-    try {
-        const sql = "insert into account (email, password) values ($1,$2) returning id"
-        const result = await query(sql, [req.body.email, req.body.password])
-        res.status(200).json({id: result.rows[0].id})
-    } catch(error) {
-        res.statusMessage = error
-        res.status(500).json({error: error})
-    }
+userRouter.post("/register", async(req, res)=> {
+        bcrypt.hash(req.body.password,10, async (err, hash) => {
+            if (!err) {
+                try {
+                    const sql = "insert into account (email, password) values ($1,$2) returning id"
+                    const result = await query(sql, [req.body.email, hash])
+                    res.status(200).json({id: result.rows[0].id})
+                } catch (error) {
+                    res.statusMessage = error
+                    res.status(500).json({error: error})
+                }
+            } else {
+                res.statusMessage = err
+                res.status(500).json({error: err})
+            }
+        })
+        
 })
 
 module.exports = {
--- shop.sql
+++ shop.sql
@@ -5,3 +5,12 @@ create table account (
 );
 
 insert into account (email, password) values ('admin@gmail.com','123456');
+
+
+create table products (
+    id serial primary key,
+    name varchar(50),
+    price float,
+    description varchar(200)
+);
+
